2cfd560b5c41ad4eea4a01c5126effb765a8bdff
alexandriatalley <alblythe47@gmail.com>
2019-02-07 15:05:02 -0700

    Worked all problems and got to pass

--- closures.js
+++ closures.js
@@ -22,13 +22,13 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
+var inner = outer();
 
 
 
 //Once you do that, invoke inner.
 
-//Code Here
+inner();
 
 
 
@@ -51,7 +51,9 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+var callJake = callFriend('Jake');
+
+console.log (callJake(435-555-9248));
 
 
 
@@ -61,16 +63,22 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter(){
+  var num = 0;
+  function count(){
+    return num += 1;
+}
+return count;
+}
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+   var count = makeCounter();
+   count(); // 1
+   count(); // 2
+   count(); // 3
+   count(); // 4
 
 
 
@@ -86,11 +94,14 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
-
   return {
-
-  };
+    inc: function (){
+      return value += 1;
+    },
+    dec: function (){
+      return value -= 1;
+    }
+  }
 }
 
 counter = counterFactory(10);
@@ -111,11 +122,13 @@ counter = counterFactory(10);
 
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
-
-  // code message function here.
+  
+  function message (){
+    return welcomeText + ' '+ firstname + ' ' + lastname + '.';
+  }
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,9 +156,13 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
+    publicMethod: function(){
+      return privateMethod();
+    }
   };
 })();
+
+module.publicMethod();
 
 
 
@@ -162,7 +179,12 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret: function(add){
+      return secret += add;
+    },
+    takeAwayFromSecret: function(take){
+      return secret -= take;
+    }
   };
 }
 
@@ -186,11 +208,10 @@ function secretNumber() {
   Fix the code below to log the desired output.
 */
 
-function timeOutCounter() {
-  for (var i = 0; i <= 5; i++) {
-    setTimeout(function() {
-      console.log(i);
-    }, i * 1000);
+
+function timeOutCounter(){
+  for (let i = 0; i<=5; i++){
+    setTimeout (()=> console.log(i), i * 1000)
   }
 }
 timeOutCounter();
